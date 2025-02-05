34655a80b5c4f0591f768a54a916b1bd6ab86475
Ivan Pouradier Duteil <ivan.pouradier-duteil@agregio-solutions.com>
2025-02-05 22:14:31 +0100

    pdf solar tracker and corresponding server

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.21-alpine AS builder
+
+WORKDIR /app
+
+COPY server.go tipe.pdf ./
+
+RUN go build -o server server.go
+
+FROM alpine:latest
+
+WORKDIR /app
+
+COPY --from=builder /app/server /app/document.pdf ./
+
+EXPOSE 8888
+
+CMD ["./server"]
---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/document", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "tipe.pdf")
+	})
+
+	log.Println("Server running on port 8888...")
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
